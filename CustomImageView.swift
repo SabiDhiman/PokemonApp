025fec358dd14c46cccf3ad1ba87fc1b380447b9
SabiDhiman <sabi98@live.co.uk>
2023-03-01 17:09:58 +0000

    added imagecache to cache images

--- CustomImageView.swift
+++ CustomImageView.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
-
+//save imagas once called
+let imageCache = NSCache<AnyObject, AnyObject>()
 
 class CustomImageView: UIImageView {
-    
+    var task: URLSessionDataTask!
     func loadImage(from url : URL) {
+        
         image = nil
+        if let task = task {
+            task.cancel()
+        }
+        //pulling images from the cache
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            self.image = imageFromCache
+            return
+        }
+        
         //define task
-        let task = URLSession.shared.dataTask(with: url){
+        task = URLSession.shared.dataTask(with: url){
             
             (data, response, error) in
             
@@ -25,6 +36,9 @@ class CustomImageView: UIImageView {
                 print("couldnt get image data ")
                 return
             }
+            //setting imagecache as new image
+            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+            
             DispatchQueue.main.async {
                 self.image = newImage
             }
